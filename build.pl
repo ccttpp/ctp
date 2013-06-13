cc08f64e49eb67d9cc552cefa4f6f7ef3a71fe27
Debian Live user <amnesia@amnesia.(none)>
2013-06-13 03:02:44 +0000

    basic proof of concept

--- build.pl
+++ build.pl
@@ -7,16 +7,64 @@ use warnings;
 
 sub build {
     my $outdir = "./site";
+    my %article_index_lists = ();
+    my $continue_processing_index = 1;
+    my $main_index_content;
+
+    $main_index_content = &read("./template/index");
+    $main_index_content =~ s/[\n\r]/ /g;
 
     `rm -rf $outdir`;
     `mkdir $outdir`;
 
-    &index($outdir, &sections($outdir));
-    &misc($outdir);
-}
+    foreach (split /\n/, `find . -regex "\./[a-z]+/[0-9]+-.*" | sort -nr -t "/" -k3`) {
+        my $filepath = $_;
+        my ($dot, $sectionname, $articlename) = split /\//, $filepath;
+        my %lookup = ();
+        my $data;
+        my $list;
+        my $fill;
 
-sub misc {
-    my ($outdir) = @_;
+        $data = &read($filepath);
+        $data =~ s/[\n\r]/ /g;
+
+        $lookup{filepath} = $filepath;
+        $lookup{filename} = substr($articlename, length("yyyymmdd-")) . ".html";
+        $lookup{section} = $sectionname;
+        $lookup{$1} = $2 while ($data =~ /<(\w+)>(.+?)<\/\1>/g);
+
+        if (exists $article_index_lists{$sectionname}) {
+            $list = $article_index_lists{$sectionname};
+        } else {
+            `mkdir $outdir/$sectionname`;
+            $list = "";
+        }
+        $list .= &fill("./template/" . $sectionname . "_item", \%lookup);
+        $article_index_lists{$sectionname} = $list;
+
+        if ($continue_processing_index and $main_index_content =~ /{$sectionname}/) {
+            $fill = &fill("./template/" . $sectionname . "_in_index", \%lookup);
+            $main_index_content =~ s/{$sectionname}/$fill/;
+        } else {
+            $continue_processing_index = 0;
+        }
+
+        open FILE, ">", "$outdir/$sectionname/$lookup{filename}" or die $!;
+        print FILE &fill("./template/" . $sectionname, \%lookup);
+        close FILE
+    }
+
+    foreach (keys %article_index_lists) {
+        open FILE, ">", "$outdir/$_/index.html" or die $!;
+        print FILE &fill("./template/" . $_ . "_index", {
+            list => $article_index_lists{$_}
+        });
+        close FILE;
+    }
+
+    open FILE, ">", "$outdir/index.html" or die $!;
+    print FILE $main_index_content;
+    close FILE;
 
     `cp ./misc/robots.txt $outdir`;
 
@@ -25,50 +73,9 @@ sub misc {
     `cp ./misc/todo.html $outdir/misc`;
 }
 
-sub index {
-    my ($outdir, $articles) = @_;
-    my @articles = @{$articles};
-    my $i = 0;
-    my $found;
-    my %seen = ();
-    my $data;
-    my %lookup = ();
-
-    open IN, "<", "./template/index" or die $!;
-    open OUT, ">", "$outdir/index.html" or die $!;
-    while (<IN>) {
-        if ($_ =~ /{(.+)}/) {
-            $found = 0;
-            for ($i = 0; !$found and $i <= $#articles; $i++) {
-                my @arr = @{${\@{$articles}}[$i]};
-
-                next if $seen{$arr[2]};
-
-                if ($arr[1] eq $1) {
-                    $found = 1;
-                    $seen{$arr[2]} = 1;
-
-                    $data = &read($arr[2]);
-                    $data =~ s/[\n\r]/ /g;
-                    while ($data =~ /<(\w+)>(.+?)<\/\1>/g) {
-                        $lookup{$1} = $2;
-                    }
-
-                    print OUT &fill("./template/$arr[1]_in_index", \%lookup);
-                }
-            }
-            die "$1 not found" if !$found;
-        } else {
-            print OUT $_;
-        }
-    }
-    close IN;
-    close OUT;
-}
-
 sub read {
     my ($file) = @_;
-
+ 
     return do { local $/ = undef; open my $fh, "<", $file or die $!; <$fh>; };
 }
 
@@ -83,73 +90,9 @@ sub fill {
 
     while ($template =~ /{(.+?)}/) {
         $key = $1;
-        die "no '$key' value in lookup" if !$lookup{$key};
+        die "'$key' value needed by $templatefile not in lookup" if !$lookup{$key};
         $template =~ s/{$key}/$lookup{$key}/;
     }
 
     return $template;
-}
-
-sub sections {
-    my ($outdir) = @_;
-    my @sections = split /\n/, `find . -type d | grep -v \\.git | grep -v site | grep -v template | grep -v misc`;
-    my @articles = ();
-
-    foreach (@sections) {
-        if ($_ =~ /^\.\/(.+)$/) { # "./sectionname"
-            foreach(@{&section($outdir, $1)}) {
-                push @articles, \@{$_};
-            }
-        }
-    }
-
-    my @sorted = reverse sort { @{$a}[0] cmp @{$b}[0] } @articles;
-
-    return \@sorted;
-}
-
-sub section {
-    my ($outdir, $sectionname) = @_;
-    my @articles = split /\n/, `find ./$sectionname -type f`;
-    my @articleinfos = ();
-    my $list = "";
-
-    `mkdir $outdir/$sectionname`;
-
-    foreach (@articles) {
-        push @articleinfos, \@{&article($outdir, $_)};
-    }
-
-    open FILE, ">", "$outdir/$sectionname/index.html" or die $!;
-    foreach (reverse sort { @{$a}[0] cmp @{$b}[0] } @articleinfos) {
-        $list .= &fill("./template/" . $sectionname . "_item", {
-            date => @{$_}[0],
-            section => @{$_}[1],
-            file => substr(@{$_}[2], length($sectionname) + 3) . ".html",
-            title => @{$_}[2]
-        });
-    }
-    print FILE &fill("./template/" . $sectionname . "_index", {
-        list => $list
-    });
-    close FILE;
-
-    return \@articleinfos;
-}
-
-sub article {
-    my ($outdir, $articlepath) = @_;
-    my ($ignore, $sectionname, $articlename) = split /\//, $articlepath; # (".", "$sectionname", "articlename")
-    my $date = "2013/";
-    my $rnd = 1 + int(12*rand());
-
-    $date .= "0" if $rnd < 10;
-    $date .= $rnd . "/";
-    $rnd = 1 + int(28*rand());
-    $date .= "0" if $rnd < 10;
-    $date .= $rnd;
-
-    `touch $outdir/$sectionname/$articlename.html`;
-
-    return [$date, $sectionname, $articlepath];
 }
